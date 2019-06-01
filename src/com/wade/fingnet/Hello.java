f9a77060ae2e2af6f0674d35b2ae282c4f3964a3
no7dw <no7david@gmail.com>
2019-06-01 19:52:56 +0800

    valid ip

--- src/com/wade/fingnet/Hello.java
+++ src/com/wade/fingnet/Hello.java
@@ -1,7 +1,9 @@
 package com.wade.fingnet;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.Socket;
 import java.net.SocketException;
 import java.util.Enumeration;
 
@@ -9,6 +11,50 @@ import java.util.Enumeration;
  * Created by dengwei on 2019/6/1.
  */
 public class Hello {
+    public static boolean validIP (String ip) {
+        try {
+            if ( ip == null || ip.isEmpty() ) {
+                return false;
+            }
+
+            String[] parts = ip.split( "\\." );
+            if ( parts.length != 4 ) {
+                return false;
+            }
+
+            for ( String s : parts ) {
+                int i = Integer.parseInt( s );
+                if ( (i < 0) || (i > 255) ) {
+                    return false;
+                }
+            }
+            if ( ip.endsWith(".") ) {
+                return false;
+            }
+
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+    public static boolean testPortOpen(String host, int port){
+        //verify host format
+        if(!validIP(host)) return false;
+        Socket socket = null;
+        try {
+            socket = new Socket(host, port);
+            return true;
+        }catch (Exception e){
+            return false;
+        }finally {
+            try{socket.close();}
+            catch (IOException e){}
+        }
+
+
+
+
+    }
     public static void main(String[] args) {
         System.out.println("Hey Babe");
         try {
@@ -22,6 +68,11 @@ public class Hello {
                     NetworkInterface net = NetworkInterface.getByInetAddress(i);
                     short n = net.getInterfaceAddresses().get(0).getNetworkPrefixLength();//0 IPV4 1 IPV6
                     System.out.println(addrInfo+"/"+n);
+                    int port = 22;
+                    boolean open =  testPortOpen(addrInfo, port);
+                    if (open) {
+                        System.out.println(port + " is open" );
+                    }
                 }
 
             }
